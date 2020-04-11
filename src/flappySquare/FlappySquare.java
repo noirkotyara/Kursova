a7f5e2350019758d0f0c2516389281b4c48e4931
Dianna <noirkotyara@gmail.com>
2020-04-11 13:12:10 +0300

    paintColumns

--- src/flappySquare/FlappySquare.java
+++ src/flappySquare/FlappySquare.java
@@ -5,38 +5,78 @@ import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Random;
 
 import javax.swing.JFrame;
 import javax.swing.Timer;
 
 public class FlappySquare implements ActionListener{
-	int ImMalukh_Diana;
+	
 	public static FlappySquare flappySquare;
 	public final int width = 400, height = 800;
 	public Render render;
 	public Rectangle bird;
+	public ArrayList<Rectangle> columns;
+	public Random random;
+	public int ticks, ymotion;
 	
 	public FlappySquare() {
 		JFrame jframe = new JFrame();
 		render = new Render();
 		Timer timer = new Timer(20, this);
-		
+		random = new Random();
 		
 		jframe.add(render);
 		jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		jframe.setSize(width, height);
-		jframe.setResizable(false);
+		jframe.setResizable(true);
 		jframe.setVisible(true);
 		
 		bird = new Rectangle(100, 200, 20, 20 );
+		columns = new ArrayList<Rectangle>();
+		
+		addColumn(true);
+		addColumn(true);
+		addColumn(true);
+		addColumn(true);
 		
 		timer.start();
 	}
+	public void addColumn(boolean a) {
+		int space = 300;
+		int Width = 70;
+		int Height = 50 + random.nextInt(300);
+		
+		if(a) {
+		columns.add(new Rectangle(width + Width + columns.size()*125 , height - Height - 145, Width, Height));
+		columns.add(new Rectangle(width + Width + (columns.size() - 1)*125,0, Width, height -Height - space ));
+	}	
+		else {
+			columns.add(new Rectangle(columns.get(columns.size() - 1).x + 100, height - Height - 145, Width, Height));
+			columns.add(new Rectangle(columns.get(columns.size() - 1).x,0, Width, height - Height - space ));
+		}
+		}
+		
+	public void paintColumn(Graphics g, Rectangle column) {
+		
+		g.setColor(Color.orange);
+		g.fillRect(column.x, column.y,column.width, column.height );
+	}
+	
 	
 	@Override
+	
+	
+	
 	public void actionPerformed(ActionEvent e) {
+		if (ticks % 2 == 0 && ymotion < 15) {
+			ymotion ++;
+		}
+		bird.y += ymotion; /// can be changed for another step of the game //bird.y -=ymotion
 		render.repaint();
 	}
+	
 	
 	
 	public void repaint(Graphics g) {
@@ -52,6 +92,10 @@ public class FlappySquare implements ActionListener{
 		
 		g.setColor(Color.black);//grass
 		g.fillRect(0, height - 150, width, 15);
+		
+		for(Rectangle column : columns) {//: in
+			paintColumn(g, column);
+		}
 	}
 	
 	
